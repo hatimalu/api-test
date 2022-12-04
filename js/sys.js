67387b7e41b27eba9ed82c07a6a8e51c933fbe97
hatimalu <github@hatimalu.net>
2022-12-04 14:33:53 +0900

    Ver 1.0

--- js/sys.js
+++ js/sys.js
@@ -1,4 +1,4 @@
-let url = "";
+let url = "https://ipapi.co/json/";
  
 fetch(url)
    .then(function(response) {
@@ -7,7 +7,9 @@ fetch(url)
    .then(function(ip) {
        console.log(ip);
        // 画面に書き出す
-       document.getElementById("ip").lastElementChild.textContent = ip.ip;
-       document.getElementById("ip").lastElementChild.textContent = ip.ip;
-       document.getElementById("ip").lastElementChild.textContent = ip.ip;
+       document.getElementById("ip").lastElementChild.textContent = ip.network;
+       document.getElementById("country").lastElementChild.textContent = ip.country_name;
+       document.getElementById("city").lastElementChild.textContent = ip.region +","+ ip.city;
+       document.getElementById("tsuusin").lastElementChild.textContent = ip.org;
+       
    });
